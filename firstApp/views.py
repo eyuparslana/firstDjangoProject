e5d14b7362cc9a6df7429eecaa509e950faed4aa
eyup arslan <eyuparslana@gmail.com>
2018-09-10 01:08:54 +0300

    basic site complated

--- firstApp/views.py
+++ firstApp/views.py
@@ -1,14 +1,43 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from .models import Post
 from django.utils import timezone
-
-
+from .forms import PostForm
 
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now())
     return render(request, '../templates/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, '../templates/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, '../templates/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, '../templates/post_edit.html', {'form': form})
